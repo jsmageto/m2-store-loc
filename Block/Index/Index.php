59ea99c9cce060360f5334ef0e412b78efffc549
Magneto Guys <xaongkir@gmail.com>
2017-04-11 10:53:02 +0700

    new submission
    new submission

--- Block/Index/Index.php
+++ Block/Index/Index.php
@@ -2,8 +2,29 @@
 
 
 namespace JM\StoreLocator\Block\Index;
+use Magento\Framework\View\Element\Template;
 
 class Index extends \Magento\Framework\View\Element\Template
 {
+	
+    protected $storeCollectionFactory;
 
+    public function __construct(
+        Template\Context $context,
+    
+        \JM\StoreLocator\Model\ResourceModel\StoreLocator\CollectionFactory $storeCollectionFactory,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+
+        $this->storeCollectionFactory = $storeCollectionFactory;
+    }
+
+	
+	public function getStoreLocations()
+	{
+		$stores = $this->storeCollectionFactory->create();
+		
+		return $stores->getData();
+	}
 }
